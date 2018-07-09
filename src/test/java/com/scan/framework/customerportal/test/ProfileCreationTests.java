699c6dc640d9e545da1cb5ad0e05bae199f0eaff
Eugene Danilovets <evgeniy.danilovets@gmail.com>
2018-07-09 18:01:05 +0300

    Second commit in my repository

---
+++ src/test/java/com/scan/framework/customerportal/test/ProfileCreationTests.java
@@ -0,0 +1,11 @@
+package com.scan.framework.customerportal.test;
+
+import org.testng.annotations.Test;
+
+public class ProfileCreationTests {
+  
+  @Test
+  public void singleProfileCreationTest(){
+  
+  }
+}
